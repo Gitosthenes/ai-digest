fe487f6d8d1db58765d6b02bbcb77d9d1e3bc072
Gitosthenes <abledso3@uw.edu>
2020-05-21 17:48:35 -0500

    add summarize endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,28 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
+import * as path from 'path';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+const app = express();
+require('dotenv').config({path: path.join(__dirname, '../../.env')});
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello World!");
+app.get('/helloWorld', (req: express.Request, res: express.Response) => {
+    res.send('Hello World');
 });
+
+app.get('/summarize', (req: express.Request, res: express.Response) => {
+    const smmry = require('smmry')({
+        SM_API_KEY: process.env.SM_API_KEY
+    });
+
+    const testURL = `https://www.npr.org/sections/alltechconsidered/2016/12/05/503581220/fake-or-real-how-to-self-check-the-news-and-get-the-facts`
+
+    smmry.summarizeUrl(testURL)
+        .then((data: string) => {
+            res.send(data);
+        })
+        .catch((err: string) => {
+            res.send(err);
+        });
+});
+
+export const api = functions.https.onRequest(app);
